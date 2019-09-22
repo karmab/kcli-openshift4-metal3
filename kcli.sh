976bf21182c3c55574b4ac01429e515d3517a8be
karmab <karimboumedhel@gmail.com>
2019-09-22 13:02:57 +0200

    render with kcli

--- kcli.sh
+++ kcli.sh
@@ -23,9 +23,13 @@ fi
 shopt -s expand_aliases
 export cluster="${cluster:-ostest}"
 export domain="${domain:-test.metalkube.org}"
+if [ ! -f ~/.ssh/id_rsa.pub ] ; then
+  echo -e "${RED}Missing ~/.ssh/id_rsa.pub${NC}"
+  exit 1
+fi
 export ssh_key=$(cat ~/.ssh/id_rsa.pub)
 if [ ! -f openshift_pull.json ] ; then
-  echo -e "Missing openshift_pull.json"
+  echo -e "${RED}Missing openshift_pull.json${NC}"
   exit 1
 fi
 export pull_secret=$(cat openshift_pull.json | tr -d [:space:])
@@ -34,5 +38,7 @@ export cidr="192.168.111.0/24"
 export dns_vip="192.168.111.3"
 export ingress_vip="192.168.111.4"
 export api_vip="192.168.111.5"
-kcli plan -f metal3.yml -P external_bridge=${external_bridge} metal3
-envsubst < install-config.templ.yaml > install-config.yaml
+export masters="${masters:-1}"
+export workers="${workers:-1}"
+kcli plan -f metal3.yml -P external_bridge=${external_bridge} -P masters=${masters} -P workers=${workers} metal3
+kcli render -f install-config.templ.yaml -P masters=$masters -P workers=$workers -P domain=$domain -P cluster=$cluster -P external_bridge=$external_bridge -P pull_secret=$pull_secret -P ssh_key=${ssh_key} > install-config.yaml
