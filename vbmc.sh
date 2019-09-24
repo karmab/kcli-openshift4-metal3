7683ae9cd342cc70ad64a0e492a7580ae8a499c0
karmab <karimboumedhel@gmail.com>
2019-09-24 15:24:07 +0200

    vbmc.sh

--- vbmc.sh
+++ vbmc.sh
@@ -6,15 +6,13 @@ sudo firewall-cmd --zone=libvirt --add-port=80/tcp >/dev/null 2>&1
 sudo firewall-cmd --zone=libvirt --add-port=80/tcp --permanent >/dev/null 2>&1
 sudo firewall-cmd --zone=libvirt --add-port=6230-6235/udp >/dev/null 2>&1
 sudo firewall-cmd --zone=libvirt --add-port=6230-6235/udp --permanent >/dev/null 2>&1
-sudo ps -ef | grep vbmcd | grep -qv grep 
-if [ "$?" != "0" ] ; then 
-  sudo mkdir /root/.vbmc 2>/dev/null || true
-  alias vbmcd='sudo podman run -d --name vbmcd  -p 6230:6230/udp -p 6231:6231/udp -p 6232:6232/udp -v /root/.vbmc:/root/.vbmc -v /var/run/libvirt:/var/run/libvirt karmab/virtualbmc-libvirt --foreground'
-  alias vbmc='sudo podman exec vbmcd vbmc'
-  shopt -s expand_aliases
-  vbmcd > /dev/null 2>&1
-fi
-for num in $(seq 0 $masters) ; do
+sudo mkdir /root/.vbmc 2>/dev/null || true
+alias vbmcd='sudo podman run -d --name vbmcd  -p 6230:6230/udp -p 6231:6231/udp -p 6232:6232/udp -v /root/.vbmc:/root/.vbmc -v /var/run/libvirt:/var/run/libvirt karmab/virtualbmc-libvirt --foreground'
+alias vbmc='sudo podman exec vbmcd vbmc'
+shopt -s expand_aliases
+vbmcd > /dev/null 2>&1
+allmasters=$(( $masters -1 ))
+for num in $(seq 0 $allmasters) ; do
  sudo ls /root/.vbmc/openshift-master-$num > /dev/null 2>&1
  if [ "$?" != "0" ] ; then 
    sudo iptables -A INPUT -p udp --dport 623$num -j ACCEPT
@@ -22,8 +20,9 @@ for num in $(seq 0 $masters) ; do
    vbmc start openshift-master-$num
  fi
 done
-for num in $(seq 0 $workers) ; do
- sudo ls /root/.vbmc/openshift-master-$num > /dev/null 2>&1
+allworkers=$(( $workers -1 ))
+for num in $(seq 0 $allworkers) ; do
+ sudo ls /root/.vbmc/openshift-workers-$num > /dev/null 2>&1
  if [ "$?" != "0" ] ; then 
    sudo iptables -A INPUT -p udp --dport 624$num -j ACCEPT
    vbmc add openshift-worker-$num --port 624$num --username admin --password password --libvirt-uri qemu:///system
